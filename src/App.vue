b387b20bdc64815497662d9fa7a9ee46ba585b38
Jack-rm <jackcnsr94@gmail.com>
2021-09-28 16:55:59 +0200

    Added axios.get function to generate random emails

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,60 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>My 10 favourite Boolean email address boolean e-mail adress are:</h1>
+    <ul>
+      <li>
+        <HelloWorld  v-for="(email,index) in randomEmail" :key="index" :email="email" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: function () {
+    return {
+      randomEmail : [],
+    }
+  },
+  methods: {
+
+    /*async getEmail(n) {
+      while (this.randomEmail.length < n) {
+        let response = await axios.get('https://flynn.boolean.careers/exercises/api/random/mail');
+        this.randomEmail.push(response.data.response);
+        console.log(response.data.response)
+      }
+    },
+    */
+   
+    getEmail(n) {
+      for (let i = 0; i < n; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+          this.randomEmail.push(response.data.response);
+          console.log(response.data.response);
+          })
+      }
+    }
+
+  },
+  mounted() {
+    console.log(axios);
+
+    /**
+     * axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+     * .then((response) => { this.randomEmail = response.data.response; });
+     */
+
+    this.getEmail(10);
   }
 }
 </script>
@@ -24,5 +67,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
 }
+
+ul li {
+  list-style-type: none;
+}
+
 </style>
